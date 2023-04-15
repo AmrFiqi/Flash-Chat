8d94ffe4c025975de2dcf1edeb432673721062f3
AmrFiqi <amr.h.elfiqi@gmail.com>
2023-04-15 16:14:53 +0200

    Created Message bubbles and cell to populate the table view

---
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,35 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Amr El-Fiqi on 15/04/2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    // MARK: - IBOutlets
+    
+    @IBOutlet var rightImageView: UIImageView!
+    @IBOutlet var label: UILabel!
+    @IBOutlet var messageBubble: UIView!
+    
+    
+    // MARK: - Class Methods
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
